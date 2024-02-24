0fbb3bf5ab11688e33dc64d526d9da7fafe1acb9
schen-atvoid <seanchenms@outlook.com>
2024-02-24 06:52:38 -0500

    Update monitor positions.

--- autostart.sh
+++ autostart.sh
@@ -10,7 +10,7 @@ function run {
 #run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 
 ######## My default monitor setting Start ########
-run xrandr --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 1920x1080 --pos 208x1440 --rotate normal
+run xrandr --output HDMI-0 --mode 1920x1080 --pos 2560x180 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
 ######## My default monitor setting End ########
 
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
