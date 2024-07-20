a1810385977feeb2462203279a2aeace024122f1
schen <seanchenms@outlook.com>
2024-07-20 14:45:31 -0400

    Update refresh rate.

--- autostart.sh
+++ autostart.sh
@@ -10,7 +10,16 @@ function run {
 #run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 
 ######## My default monitor setting Start ########
-xrandr --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 3840x266 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
+
+xrandr --output HDMI-0 --off \
+       --output DP-0 --mode 2560x1440 --rate 59.95 --pos 3840x266 --rotate normal \
+       --output DP-1 --off \
+       --output HDMI-1 --off \
+       --output DP-2 --primary --mode 3840x2160 --rate 143.96 --pos 0x0 --rotate normal \
+       --output DP-3 --off \
+       --output DP-4 --off \
+       --output DP-5 --off
+
 ######## My default monitor setting End ########
 
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
